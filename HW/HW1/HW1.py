d38e85cd7839803b7e60bc22718ce0823aff2eaf
Aaron <aronton0502@gmail.com>
2023-11-13 11:51:27 +0800

    36

--- HW/HW1/HW1.py
+++ HW/HW1/HW1.py
@@ -1,73 +1,118 @@
-print("Welcome to PyMoney ! How much money do you have?")
-global money = int(input())
-print("Initial Money is", money)
-global record = []
-continueOrNot = input("If you want to add or take money, key Y, N to exist \n")
+import os
+
+global money 
+global record 
+
+money = 0
+record = []
 
 def initialize():
+    global money 
+    global record 
     print("Welcome to PyMoney ! How much money do you have?")
     money = int(input())
-    print("Initial Money is", money)
-    record = []    
-    return
-
-def delete():
-    return
-
-def view():
+    record.clear()
     print("Description          Amount\n")
     print("==================== ======\n")
-    for i in record:
-        print( "%s                  %d\n", i[0], i[1])
+    record.append(("Initail Money", money))
+    print("Initail Money        %d\n" %money)
+
+def view():
+    global money 
+    global record 
+    print("Description          Amount\n")
     print("==================== ======\n")
-    print("%d\n",money)
+    for item in record:
+        print( "%15s%12s\n" %(item[0], item[1]))
+    print("==================== ======\n")
+    localMoney = 0
+    for item in record:
+        localMoney = localMoney + int(item[1])
+    money = localMoney
+    print("tot:%d\n" %money)
 
 
 def add():
-    add = input("How much you add ( ex lunch 10 )\n")
-    item = add.split(" ",1)[0]
-    add = add.split(" ",1)[1]
-    record.append((item,add))
-    if(add[0] != "-"):
-        # add = input("How much you add ( ex lunch 10 )\n")
-        # item = add.split(" ",1)[0]
-        # itemPrice = int(add.split(" ",1)[1])
-        money = money + int(add[:])
-    if(add[0] == "-"):
-        # take = input("How much you take ( ex lunch -10 or lunch 10 )\n")
-        # item = take.split(" ",1)[0]
-        # take = take.split(" ",1)[1]
-        money = money - int(add[1:])
-        # if(take[0] == "-"):
-        #     take = take.split("-",1)[1]
-        #     money = money - int(take)
-        # else:
-        #     money = money - int(take)
-        # item = take.split(" ",1)[0]
-        # itemPrice = int(take.split(" ",1)[1])
-def save():
+    global money 
+    global record 
+    while True:
+        add = input("How much you add ( ex lunch 10, exit to end )\n")
+        if(add == "exit"):
+            break
+        item = add.split(" ",1)[0]
+        add = add.split(" ",1)[1]
+        record.append((item,add))
+        if(add[0] != "-"):
+            money = money + int(add[:])
+        if(add[0] == "-"):
+            money = money - int(add[1:])
+# def delete(record):
+
+def delete():
+    global record 
+    print("choose the number you want to delete, ex 1,2,3")
+    idx = int(input())
+    del record[idx]
+    return        
+# def save(money,record):
+
+
+def save(name):
+    global money 
+    global record 
+    file = open("./" + name + ".txt","w")
+    for item in record:
+        file.write("%s            %s\n" %(item[0], item[1]))
+    file.write("Tot:%s\n" %(money))
+    file.close()
+    return
+
+def read(name):
+    global money 
+    global record 
+    record.clear()
+    file = open("./" + name + ".txt","r")
+    fileList = []
+    fileList = file.readlines()
+    print(fileList)
+    print("\n")
+    for i in range(len(fileList)-1):
+        if(i == 0):
+            a = fileList[i].split()[0]
+            b = fileList[i].split()[1]
+            c = fileList[i].split()[2]
+            record.append((a + " " + b,c))
+        else:
+            a = fileList[i].split()[0]
+            b = fileList[i].split()[1]
+            record.append((a,b))
+    # file.write("Tot:%s\n" %(money))
+    file.close()
     return
 
 
-while (continueOrNot == "Y"):
-    # initialize()
-    # a = input("add or take ? key Y or N\n")
-    # add = input("How much you add ( ex lunch 10 )\n")
-    # item = add.split(" ",1)[0]
-    # add = add.split(" ",1)[1]
-    # record = []
-    # record.append((item,add))
-    action = input("what do you want?(initialize, add, view, delete, save)")
-    if(action == "initialize"):
+while True:
+    action = input("what do you want?( (a) initialize, (b) add, (c) view, (d) delete, (e) save, (f) read (g) exit, ex:a,b...)")
+    print("action:",action)
+    if(action == "a"):
         initialize()
-    elif(action == "add"):
+        # initialize(money, record)
+    elif(action == "b"):
         add()
-    elif(action == "view"):
+        # add(money, record)
+    elif(action == "c"):
         view()
-    elif(action == "delete"):
+        # view(money,record)
+    elif(action == "d"):
         delete()
-    elif(action == "save"):
-        save()
-    # print("Current Money is", money)
-    # print("Record is", record)
-    continueOrNot = input("If you want to continue to add or take money, key Y\n")
+        # delete(record)
+    elif(action == "e"):
+        name = input("Enter fileName : ")
+        save(name)
+        # save(money,record)
+    elif(action == "f"):
+        name = input("Enter fileName : ")
+        read(name)
+    elif(action == "g"):
+        break
+view()
